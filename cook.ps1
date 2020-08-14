37a2b53fd1bd4204f409ad55c5396fcc844e2b86
Jinming Wu, Patrick <me@patrickwu.space>
2020-08-14 12:33:30 +0800

    use try/finally to solve failures

--- cook.ps1
+++ cook.ps1
@@ -12,7 +12,7 @@ Param (
 
 $arch_linux2win = @{ amd64 = "x64"; arm64 = "ARM64" }
 #importing definitions to be used for build
-Write-Host "# Checking manual..." -ForegroundColor DarkYellow
+Write-Host "# Checking power..." -ForegroundColor DarkYellow
 
 $ARamdomTable = Import-Csv .\def.csv | Where-Object rel -eq "$Release"
 $ReleaseChannel = $ARamdomTable.rel
@@ -28,55 +28,56 @@ Write-Host "# Executable Name: $ExecName.exe" -ForegroundColor Green
 Write-Host "# Full Name: $FullName" -ForegroundColor Green
 
 # checking whether these executables exist
-Write-Host "# Checking power..." -ForegroundColor DarkYellow
+Write-Host "# Getiing cooker ready..." -ForegroundColor DarkYellow
 
 Import-Module C:\Users\Patrick\Git\PsUWI\PsUWI.psd1
 #Import-Module PsUWI
 
-foreach ($item in @('git.exe', 'wget.exe')) {
-    if ((Get-Command "$item" -ErrorAction SilentlyContinue) -eq $null) { 
-        Write-Host "Unable to find $item in your PATH"
-        exit 1
+$build_instance = New-UbuntuWSLInstance -Release focal -Version 2 -AdditionalPkg "git,wget,make,icoutils,inkscape" -NonInteractive
+wsl.exe -d ubuntu-$build_instance echo -e `"`[automount`]\noptions `= `"metadata`"`" `>`> /etc/wsl.conf
+wsl.exe -t ubuntu-$build_instance
+function Run-WithInstance {wsl.exe -d ubuntu-$build_instance -u $env:USERNAME $args}
+
+try {
+    # getting the WSL Distro Launcher source
+    Write-Host "# Putting Rice..." -ForegroundColor DarkYellow
+    Run-WithInstance git clone $LauncherUrl launcher
+
+    git.exe clone $IngredientUrl ingredients
+    # Run-WithInstance git clone $IngredientUrl ingredients
+    Set-Location ./ingredients
+    Run-WithInstance DESTDIR=../launcher clean_remote
+    Run-WithInstance make
+    Run-WithInstance DESTDIR=../launcher make install
+    Set-Location ..
+
+    Write-Host "# Putting Water..." -ForegroundColor DarkYellow
+    # foreach ($item in @('SHA256SUMS', 'SHA256SUMS.gpg')) {
+    #     wget.exe $BaseImgUrl/$Release/current/$item
+    # }
+    # gpg.exe --verify SHA256SUMS.gpg SHA256SUMS
+
+    foreach ($arch in @('amd64', 'arm64')) {
+        if ( ( $Release -eq "xenial" ) -and ( $arch -eq "arm64" )) {
+            break
+        }
+        $ArchFolderName = $arch_linux2win["$arch"]
+        if ( -not (Test-Path -Path ".\launcher\$ArchFolderName" -PathType Container ) ) {
+            mkdir -Path ".\launcher\$ArchFolderName" | Out-Null
+        }
+        Run-WithInstance wget $BaseImgUrl/$Release/current/$Release-server-cloudimg-$arch-wsl.rootfs.tar.gz
+        Move-Item -Force $Release-server-cloudimg-$arch-wsl.rootfs.tar.gz .\launcher\$ArchFolderName\install.tar.gz
     }
+
+    Write-Host "# Rinsing Rice..." -ForegroundColor DarkYellow
+
+    (Get-Content .\launcher\DistroLauncher.sln).replace('"DistroLauncher-Appx"', '"Ubuntu"') | Set-Content .\launcher\DistroLauncher.sln
+    (Get-Content .\launcher\DistroLauncher.sln).replace('DistroLauncher-Appx.', 'Ubuntu.') | Set-Content .\launcher\DistroLauncher.sln
+    #(Get-Content .\launcher\DistroLauncher.sln).replace('DistroLauncher-Appx.', 'Ubuntu.') | Set-Content .\launcher\DistroLauncher.sln
+}
+finally {
+    Remove-UbuntuWSLInstance -Id $build_instance
+    # Remove-Item -Force -Recurse ingredients
+    # Remove-Item -Force -Recurse launcher
 }
 
-# getting the WSL Distro Launcher source
-Write-Host "# Putting Rice..." -ForegroundColor DarkYellow
-git.exe clone $LauncherUrl launcher
-
-git.exe clone $IngredientUrl ingredients
-$build_instance = New-UbuntuWSLInstance -Release focal -Version 2 -AdditionalPkg "make,icoutils,inkscape" -NonInteractive
-Set-Location ./ingredients
-wsl.exe -d ubuntu-$build_instance -u $env:USERNAME DESTDIR=../launcher clean_remote
-wsl.exe -d ubuntu-$build_instance -u $env:USERNAME make
-wsl.exe -d ubuntu-$build_instance -u $env:USERNAME DESTDIR=../launcher make install
-Remove-UbuntuWSLInstance -Id $build_instance
-Set-Location ..
-
-
-Write-Host "# Putting Water..." -ForegroundColor DarkYellow
-# foreach ($item in @('SHA256SUMS', 'SHA256SUMS.gpg')) {
-#     wget.exe $BaseImgUrl/$Release/current/$item
-# }
-# gpg.exe --verify SHA256SUMS.gpg SHA256SUMS
-
-foreach ($arch in @('amd64', 'arm64')) {
-    if ( ( $Release -eq "xenial" ) -and ( $arch -eq "arm64" )) {
-        break
-    }
-    $ArchFolderName = $arch_linux2win["$arch"]
-    if ( -not (Test-Path -Path ".\launcher\$ArchFolderName" -PathType Container ) ) {
-        mkdir -Path ".\launcher\$ArchFolderName" | Out-Null
-    }
-    wget.exe $BaseImgUrl/$Release/current/$Release-server-cloudimg-$arch-wsl.rootfs.tar.gz
-    Move-Item -Force $Release-server-cloudimg-$arch-wsl.rootfs.tar.gz .\launcher\$ArchFolderName\install.tar.gz
-}
-
-Write-Host "# Rinsing Rice..." -ForegroundColor DarkYellow
-
-(Get-Content .\launcher\DistroLauncher.sln).replace('"DistroLauncher-Appx"', '"Ubuntu"') | Set-Content .\launcher\DistroLauncher.sln
-(Get-Content .\launcher\DistroLauncher.sln).replace('DistroLauncher-Appx.', 'Ubuntu.') | Set-Content .\launcher\DistroLauncher.sln
-
-
-# Remove-Item -Force -Recurse ingredients
-# Remove-Item -Force -Recurse launcher
