1106cae4a2b0e55c3fcf094871a63c66767a7188
Jinming Wu, Patrick <me@patrickwu.space>
2020-10-30 18:43:42 +0800

    modulize sign function

--- cook.ps1
+++ cook.ps1
@@ -164,8 +164,7 @@ try {
     Write-Host "# Cooking Rice..." -ForegroundColor DarkYellow
 
     if ( -not (Test-Path -Path ".\launcher\DistroLauncher-Appx\DistroLauncher-Appx_TemporaryKey.pfx" -PathType Leaf) ) {
-        & 'C:\Program Files (x86)\Windows Kits\10\bin\x64\makecert.exe' -r -h 0 -n "CN=23596F84-C3EA-4CD8-A7DF-550DCE37BCD0" -eku "1.3.6.1.5.5.7.3.3,1.3.6.1.4.1.311.10.3.13" -pe -sv .\launcher\DistroLauncher-Appx\DistroLauncher-Appx_TemporaryKey.pvk .\launcher\DistroLauncher-Appx\DistroLauncher-Appx_TemporaryKey.cer
-        & 'C:\Program Files (x86)\Windows Kits\10\bin\x64\pvk2pfx' -pvk .\launcher\DistroLauncher-Appx\DistroLauncher-Appx_TemporaryKey.pvk -spc .\launcher\DistroLauncher-Appx\DistroLauncher-Appx_TemporaryKey.cer -pfx .\launcher\DistroLauncher-Appx\DistroLauncher-Appx_TemporaryKey.pfx
+        ./make.ps1 create-sign
     }
 
     foreach ($arch in @('amd64', 'arm64')) {
