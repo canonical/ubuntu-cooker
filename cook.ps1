a8d1168b977dfc010ea76648eac01c5bf971e4f4
Jinming Wu, Patrick <me@patrickwu.space>
2021-01-15 14:10:18 +0800

    modulize ricing

--- cook.ps1
+++ cook.ps1
@@ -134,49 +134,9 @@ try {
 
     Write-Host "# Rinsing Rice..." -ForegroundColor DarkYellow
 
-    # root folder
-    Find-AndReplace .\launcher\DistroLauncher.sln '"DistroLauncher-Appx"' '"Ubuntu"'
-    Find-AndReplace .\launcher\DistroLauncher.sln 'DistroLauncher-Appx.' 'Ubuntu.'
-
-    # DistroLauncher
-    Copy-Item -Recurse -Force .\ingredients\sourcecode\* .\launcher\DistroLauncher\
-    Find-AndReplace .\launcher\DistroLauncher\DistributionInfo.h 'MyDistribution' "$RegName"
-    Find-AndReplace .\launcher\DistroLauncher\DistributionInfo.h 'My Distribution' "$FullName"
-    Find-AndReplace .\launcher\DistroLauncher\DistroLauncher.vcxproj 'v142' 'v141'
-
-    #DistroLauncherAppx
-    if ( Test-Path -Path ".\ingredients\temp.key" -PathType Container ) {
-        Copy-Item -Recurse -Force .\ingredients\temp.key\* .\launcher\DistroLauncher-Appx\
+    if ( Test-Path -Path ".\ingredients\rice.ps1" -PathType Leaf ) {
+        .\ingredients\rice.ps1
     }
-    Copy-Item -Recurse -Force ".\ingredients\package.asso\$ReleaseChannel.xml" .\launcher\DistroLauncher-Appx\Package.StoreAssociation.xml
-    
-    Rename-Item -Path .\launcher\DistroLauncher-Appx\DistroLauncher-Appx.vcxproj -NewName Ubuntu.vcxproj
-    Rename-Item -Path .\launcher\DistroLauncher-Appx\DistroLauncher-Appx.vcxproj.filters -NewName Ubuntu.vcxproj.filters
-
-    Find-AndReplace .\launcher\DistroLauncher-Appx\Ubuntu.vcxproj.filters 'MyDistro.appxmanifest' '$(Platform)/Ubuntu.appxmanifest'
-
-    Find-AndReplace .\launcher\DistroLauncher-Appx\Ubuntu.vcxproj 'v142' 'v141'
-    Find-AndReplace .\launcher\DistroLauncher-Appx\Ubuntu.vcxproj 'MyDistro.appxmanifest' '$(Platform)/Ubuntu.appxmanifest'
-    Find-AndReplace .\launcher\DistroLauncher-Appx\Ubuntu.vcxproj '10.0.16215.0' '10.0.16237.0'
-    Find-AndReplace .\launcher\DistroLauncher-Appx\Ubuntu.vcxproj 'MyDistro.appxmanifest' '$(Platform)/Ubuntu.appxmanifest'
-    Find-AndReplace .\launcher\DistroLauncher-Appx\Ubuntu.vcxproj 'mydistro' "$ExecName"
-    Find-AndReplace .\launcher\DistroLauncher-Appx\Ubuntu.vcxproj '<ProjectName>DistroLauncher-Appx</ProjectName>' '<ProjectName>Ubuntu</ProjectName>'
-    Find-AndReplace .\launcher\DistroLauncher-Appx\Ubuntu.vcxproj '<AppxAutoIncrementPackageRevision>True</AppxAutoIncrementPackageRevision>' ''
-    Find-AndReplace .\launcher\DistroLauncher-Appx\Ubuntu.vcxproj "<Command>copy `$(SolutionDir)\`$(platform)\`$(Configuration)\launcher.exe `$(SolutionDir)\`$(platform)\`$(Configuration)\`$(ProjectName)\`$(targetname).exe</Command>" "<Command>copy `$(OutDir)\..\launcher.exe `$(OutDir)\`$(targetname).exe</Command>"
-    Find-AndInsertAfter .\launcher\DistroLauncher-Appx\Ubuntu.vcxproj '<AppxBundlePlatforms>x64|arm64</AppxBundlePlatforms>' '    <AppxSymbolPackageEnabled>True</AppxSymbolPackageEnabled>'
-
-    Find-AndReplace .\launcher\DistroLauncher-Appx\MyDistro.appxmanifest ' Name="WSL-DistroLauncher"' " Name=`"$PkgName`""
-    Find-AndReplace .\launcher\DistroLauncher-Appx\MyDistro.appxmanifest 'Version="1.0.0.0"' "Version=`"$PkgVersion`""
-    Find-AndReplace .\launcher\DistroLauncher-Appx\MyDistro.appxmanifest 'CN=Windows Console Dev Team' 'CN=23596F84-C3EA-4CD8-A7DF-550DCE37BCD0'
-    Find-AndReplace .\launcher\DistroLauncher-Appx\MyDistro.appxmanifest 'ProcessorArchitecture="x64"' 'ProcessorArchitecture="ARCHITECTUREPLACEHOLDER"'
-    Find-AndReplace .\launcher\DistroLauncher-Appx\MyDistro.appxmanifest 'WSL-DistroLauncher' "$FullName"
-    Find-AndReplace .\launcher\DistroLauncher-Appx\MyDistro.appxmanifest 'Windows Console Dev Team' 'Canonical Group Limited'
-    Find-AndReplace .\launcher\DistroLauncher-Appx\MyDistro.appxmanifest 'mydistro' "$ExecName"
-    Find-AndReplace .\launcher\DistroLauncher-Appx\MyDistro.appxmanifest 'My Own Distro Launcher' "$FullName on Windows"
-    Find-AndInsertAfter .\launcher\DistroLauncher-Appx\MyDistro.appxmanifest '<uap:DefaultTile Wide310x150Logo="Assets\Wide310x150Logo.png" Square310x310Logo="Assets\LargeTile.png" Square71x71Logo="Assets\SmallTile.png">' '<uap:ShowNameOnTiles><uap:ShowOn Tile="square150x150Logo" /></uap:ShowNameOnTiles>'
-    Find-AndReplace .\launcher\DistroLauncher-Appx\MyDistro.appxmanifest '<uap:DefaultTile Wide310x150Logo="Assets\Wide310x150Logo.png" Square310x310Logo="Assets\LargeTile.png" Square71x71Logo="Assets\SmallTile.png">' "<uap:DefaultTile Wide310x150Logo=`"Assets\Wide310x150Logo.png`" Square310x310Logo=`"Assets\LargeTile.png`" Square71x71Logo=`"Assets\SmallTile.png`" ShortName=`"$FullName`">"
-    Find-AndReplace .\launcher\DistroLauncher-Appx\MyDistro.appxmanifest 'transparent' '#E95420'
-    Find-AndReplace .\launcher\DistroLauncher-Appx\MyDistro.appxmanifest '10.0.16215.0' '10.0.16237.0'
 
     # preparing the build!
     foreach ($arch in @('amd64', 'arm64')) {
